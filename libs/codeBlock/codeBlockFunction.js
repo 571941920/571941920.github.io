ed11775c9aa05029c112963e92db4a8a05763b37
“571941920” <571941920@163.com>
2021-10-20 21:21:38 +0800

    Site updated: 2021-10-20 21:21:37

---
+++ libs/codeBlock/codeBlockFunction.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-20 12:54:36
+ * @LastEditTime: 2021-10-20 12:54:55
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \undefinedc:\Users\57194\Desktop\Blog\themes\hexo-theme-matery\source\codeBlock\codeBlockFunction.js
+ */
+// 代码块功能依赖
+$(function () {
+    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
+});
--- libs/codeBlock/codeShrink.js
+++ libs/codeBlock/codeShrink.js
@@ -1,16 +1,23 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-20 12:58:01
+ * @LastEditTime: 2021-10-20 12:58:15
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \undefinedc:\Users\57194\Desktop\Blog\themes\hexo-theme-matery\source\libs\codeBlock\codeShrink.js
+ */
 // 代码块收缩
-
 $(function () {
-  var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
-
-  $('.code-area').prepend($code_expand);
-  $('.code-expand').on('click', function () {
-    if ($(this).parent().hasClass('code-closed')) {
-      $(this).siblings('pre').find('code').show();
-      $(this).parent().removeClass('code-closed');
-    } else {
-      $(this).siblings('pre').find('code').hide();
-      $(this).parent().addClass('code-closed');
-    }
+    var $code_expand = $('<i class="fa fa-angle-down code-expand" aria-hidden="true"></i>');
+  
+    $('.code-area').prepend($code_expand);
+    $('.code-expand').on('click', function () {
+      if ($(this).parent().hasClass('code-closed')) {
+        $(this).siblings('pre').find('code').show();
+        $(this).parent().removeClass('code-closed');
+      } else {
+        $(this).siblings('pre').find('code').hide();
+        $(this).parent().addClass('code-closed');
+      }
+    });
   });
-});
